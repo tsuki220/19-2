126ceca480ae00a7920c70af23fefe0b8c31ee57
tsuki220 <zm18404@josai.ac.jp>
2020-01-28 05:00:29 +0000

    1/28

--- ex06-9.js
+++ ex06-9.js
@@ -2,11 +2,6 @@ function txtchange1(){
     document.getElementByld(`txt1`).innerHTML=`ナンバーワン！`；
 ｝
 
-<div id="image1">
-<input type="button" value="画像の変更"
-onclick="imgchange1();">
-<img src=`./image/josai.png` id="logo" alt="Josai
-Logo"></div>
 function imgchange1(){
     document.getElementByld(`logo`).src =
     `.image/bulbon.gif`;
